d485cdc23956be2961b2c133e8e0ee65a02dbeed
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-04-19 13:22:26 +0530

    13. Kafka Producer Send Message Asynchronously

--- ProductsMicroservice/src/main/java/com/wchamarakafka/ws/products/service/ProductServiceImpl.java
+++ ProductsMicroservice/src/main/java/com/wchamarakafka/ws/products/service/ProductServiceImpl.java
@@ -1,12 +1,45 @@
 package com.wchamarakafka.ws.products.service;
 
 import com.wchamarakafka.ws.products.rest.CreateProductResModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    public ProductServiceImpl(KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     @Override
     public String createProduct(CreateProductResModel createProductResModel) {
-        return "";
+
+        String productId = UUID.randomUUID().toString();
+//        TODO: Implement the logic to save the product in the database
+
+        ProductCreatedEvent productCreatedEvent = new ProductCreatedEvent(productId,
+                createProductResModel.getTitle(), createProductResModel.getPrice(),
+                createProductResModel.getQuantity());
+
+        CompletableFuture<SendResult<String, ProductCreatedEvent>> future = kafkaTemplate.send("product-created-events-topic", productId, productCreatedEvent);
+
+        future.whenComplete((sendResult, throwable) -> {
+            if (throwable == null) {
+                logger.info("Product created event sent successfully: {}", sendResult.getProducerRecord().value());
+                logger.info("Product MetaData: {}", sendResult.getRecordMetadata());
+            } else {
+                System.out.println("Error sending product created event: " + throwable.getMessage());
+            }
+        });
+        future.join();
+        return productId;
     }
 }
